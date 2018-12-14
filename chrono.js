27f4bd5c196f5b07d265b08fa48480192cab9804
Rémi <ponche62880@gmail.com>
2018-12-15 00:41:20 +0100

    add sytem hidden button

--- chrono.js
+++ chrono.js
@@ -1,73 +1,65 @@
-let bigBen; 
-let timeStart; 
-let timeChrono = new Date; 
-let tagChronoTime = document.querySelector("#timeChrono span") ; 
-let theSandTime ; 
+let bigBen;
+let timeStart;
+let timeChrono = new Date;
+let tagChronoTime = document.querySelector("#timeChrono span");
+let theSandTime;
 
-let tagButtonStart = document.querySelector("#buttonStart") ; 
-let tagButtonReset = document.querySelector("#buttonReset") ; 
-let tagButtonRun = document.querySelector("#buttonRun") ; 
-let tagButtonStop = document.querySelector("#buttonStop") ; 
+let tagButtonStart = document.querySelector("#buttonStart");
+let tagButtonReset = document.querySelector("#buttonReset");
+let tagButtonRun = document.querySelector("#buttonRun");
+let tagButtonStop = document.querySelector("#buttonStop");
 
-let hoursTime ; 
-let minutesTime ; 
-let secondesTime ; 
-let milliSecondeTime ; 
+let hoursTime;
+let minutesTime;
+let secondesTime;
+let milliSecondeTime;
 
-function resetChrono()
-{
+function resetChrono() {
     // a définir 
 }
-function stopChrono()
-{
-    clearInterval(theSandTime) ; 
-    theSandTime = undefined ;  
-    
+function stopChrono() {
+    clearInterval(theSandTime);
+    theSandTime = undefined;
+
 }
 
-function startChrono()
-{
+function startChrono() {
     // suppresion de intervall si déja un en cours  
-    if(theSandTime != undefined)
-    {
+    if (theSandTime != undefined) {
         clearInterval(theSandTime);
-        theSandTime = undefined ; 
+        theSandTime = undefined;
     }
-    timeStart = new Date; 
-    theSandTime = setInterval(updateChrono, 100) ; 
+    timeStart = new Date;
+    theSandTime = setInterval(updateChrono, 100);
 }
-function runChrono()
-{
+function runChrono() {
     // on change le temps du départ 
-    if(timeStart == undefined)
-    {
+    if (timeStart == undefined) {
         // demare le chrono a UTC 0 
-        startChrono() ; 
+        startChrono();
     }
-    bigBen = new Date ; 
-    timeStart.setTime(bigBen.getTime() - timeChrono.getTime()) ; 
+    bigBen = new Date;
+    timeStart.setTime(bigBen.getTime() - timeChrono.getTime());
     // suppresion de intervall si déja un en cours  
-    if(theSandTime != undefined)
-    {
+    if (theSandTime != undefined) {
         clearInterval(theSandTime);
-        theSandTime = undefined ; 
+        theSandTime = undefined;
     }
-    theSandTime = setInterval(updateChrono, 100) ; 
+    theSandTime = setInterval(updateChrono, 100);
 }
 
-function updateChrono()
-{
-    bigBen = new Date; 
-    timeChrono = new Date(bigBen.getTime() - timeStart.getTime()); 
+function updateChrono() {
+    bigBen = new Date;
+    timeChrono = new Date(bigBen.getTime() - timeStart.getTime());
 
 
     // ajoute des zero dans les valeur si neccessaire 
-    hoursTime = timeChrono.getUTCHours() < 10 ? "0" + timeChrono.getUTCHours() : timeChrono.getUTCHours() ; 
-    minutesTime = timeChrono.getMinutes() < 10 ? "0" + timeChrono.getMinutes() : timeChrono.getMinutes() ; 
-    secondesTime = timeChrono.getSeconds() < 10 ? "0" + timeChrono.getSeconds() : timeChrono.getSeconds() ; 
-    milliSecondeTime = timeChrono.getSeconds() < 10 ? "0" + timeChrono.getMilliseconds() : timeChrono.getMilliseconds() ; 
+    hoursTime = timeChrono.getUTCHours() < 10 ? "0" + timeChrono.getUTCHours() : timeChrono.getUTCHours();
+    minutesTime = timeChrono.getMinutes() < 10 ? "0" + timeChrono.getMinutes() : timeChrono.getMinutes();
+    secondesTime = timeChrono.getSeconds() < 10 ? "0" + timeChrono.getSeconds() : timeChrono.getSeconds();
+    milliSecondeTime = timeChrono.getSeconds() < 10 ? "0" + timeChrono.getMilliseconds() : timeChrono.getMilliseconds();
 
-    tagChronoTime.innerHTML = hoursTime + ":" + minutesTime + ":" + secondesTime ; 
+    tagChronoTime.innerHTML = hoursTime + ":" + minutesTime + ":" + secondesTime;
 
 
 
@@ -75,20 +67,33 @@ function updateChrono()
 
 
 // ajout des Event Listener 
-tagButtonStart.addEventListener("click", function(){
-    startChrono() ; 
+tagButtonStart.addEventListener("click", function () {
+    startChrono();
+    tagButtonStart.classList.add("alpha");
+    tagButtonStop.classList.remove("alpha");
 });
 
-tagButtonStop.addEventListener("click", function(){
-    stopChrono() ; 
-}); 
-
-tagButtonReset.addEventListener("click", function(){
-    resetChrono() ; 
+tagButtonStop.addEventListener("click", function () {
+    stopChrono();
+    tagButtonStop.classList.add("alpha");
+    tagButtonRun.classList.remove("alpha");
+    tagButtonReset.classList.remove("alpha");
 });
 
-tagButtonRun.addEventListener("click", function(){
-    runChrono() ; 
-}); 
+tagButtonReset.addEventListener("click", function () {
+    resetChrono();
+    tagButtonReset.classList.add("alpha");
+    tagButtonRun.classList.add("alpha");
+    tagButtonStart.classList.remove("alpha");
+
+});
+
+tagButtonRun.addEventListener("click", function () {
+    runChrono();
+    tagButtonRun.classList.add("alpha");
+    tagButtonReset.classList.add("alpha");
+    tagButtonStop.classList.remove("alpha");
+
+});
 
 
