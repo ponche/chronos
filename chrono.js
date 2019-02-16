8607b6bdf768f149c3740fbae033808490b3b9d8
Rémi <ponche62280@gmail.com>
2019-02-16 13:01:32 +0100

    add function deleteCourse

--- chrono.js
+++ chrono.js
@@ -2,20 +2,20 @@ let bigBen;
 let timeStart;
 let timeChrono = new Date;
 let tagChronoTime = document.querySelector("#timeChrono span");
-let tagPrintZone = document.querySelector("#printZone") ; 
+let tagPrintZone = document.querySelector("#printZone");
 let theSandTime;
 
 let tagButtonStart = document.querySelector("#buttonStart");
 let tagButtonReset = document.querySelector("#buttonReset");
 let tagButtonRun = document.querySelector("#buttonRun");
 let tagButtonStop = document.querySelector("#buttonStop");
-let tagInputNameCourse = document.querySelector("#inputNameCourse") ; 
-let tagButtonSubmitSave = document.querySelector("#buttonSubmitSaveCourse") ; 
+let tagInputNameCourse = document.querySelector("#inputNameCourse");
+let tagButtonSubmitSave = document.querySelector("#buttonSubmitSaveCourse");
 
 // variable lock reset before reset 
-let savedCourse = false ; 
+let savedCourse = false;
 
-let listeCourse = [] ; 
+let listeCourse = [];
 
 let hoursTime;
 let minutesTime;
@@ -24,18 +24,17 @@ let milliSecondeTime;
 
 function resetChrono() {
     // si l'utlisateur n'as pas save, on demande si veut faire faire le reset 
-    if(!savedCourse)
-    {
-        if(!confirm("Vous n'avez pas sauvegarde la course, etes vous sur du Reset ? "))
-            return ; 
-             
+    if (!savedCourse) {
+        if (!confirm("Vous n'avez pas sauvegarde la course, etes vous sur du Reset ? "))
+            return;
+
     }
     // on change les button 
     tagButtonReset.classList.add("alpha");
     tagButtonRun.classList.add("alpha");
     tagButtonStart.classList.remove("alpha");
     // on remet le compteur à zéro 
-    tagChronoTime.innerHTML = "00:00:00" ; 
+    tagChronoTime.innerHTML = "00:00:00";
 }
 function stopChrono() {
     clearInterval(theSandTime);
@@ -45,7 +44,7 @@ function stopChrono() {
 
 function startChrono() {
     // le chrono demarre on considère qui n'as pas fait de sauvegarde 
-    savedCourse = false ; 
+    savedCourse = false;
     // suppresion de intervall si déja un en cours  
     if (theSandTime != undefined) {
         clearInterval(theSandTime);
@@ -87,59 +86,73 @@ function updateChrono() {
 
 }
 
-// sauveggarde course 
-function saveCourse()
-{
-    let nameCourse = tagInputNameCourse.value ; 
-    let temps = timeChrono ; 
-    listeCourse.push({name: nameCourse, time: temps}) ; 
-    localStorage.setItem("dataCourse", JSON.stringify(listeCourse)) ; 
-    tagInputNameCourse.value = "" ; 
-    savedCourse = true ; 
+// save course in array
+function saveCourse() {
+    let nameCourse = tagInputNameCourse.value;
+    let temps = timeChrono;
+    listeCourse.push({ name: nameCourse, time: temps });
+    localStorage.setItem("dataCourse", JSON.stringify(listeCourse));
+    tagInputNameCourse.value = "";
+    savedCourse = true;
+    loadDataCourses();
+}
+function deleteCourse(index) {
+    listeCourse.splice(index, 1);
+    localStorage.setItem("dataCourse", JSON.stringify(listeCourse));
+    drawListCourse();
 }
 
-function loadDataCourses()
-{
-    listeCourse = JSON.parse(localStorage.getItem("dataCourse"));
+
+
+function loadDataCourses() {
+    dataStorage = JSON.parse(localStorage.getItem("dataCourse"));
+    if (dataStorage != null) {
+        listeCourse = dataStorage;
+    }
+    drawListCourse();
 }
-function createTagCourseTime(nameCourse, timeCourse)
-{
-    var tagScoreCourse = document.createElement("div"); 
-    tagScoreCourse.classList.add("scoreCourse") ; 
+function createTagCourseTime(nameCourse, timeCourse, idCourse) {
+    var tagScoreCourse = document.createElement("div");
+    tagScoreCourse.classList.add("scoreCourse");
 
     // trash logo 
     var tagTrashButton = document.createElement("img");
-    tagTrashButton.src = "img/logoTrash.png" ; 
-    tagTrashButton.alt = "logo trash" ; 
-    tagTrashButton.classList.add("logo-trash") ; 
+    tagTrashButton.src = "img/logoTrash.png";
+    tagTrashButton.alt = "logo trash";
+    tagTrashButton.id = idCourse;
+    tagTrashButton.classList.add("logo-trash");
 
     // course name 
-    var tagNameCourse = document.createElement("span") ; 
-    tagNameCourse.innerHTML = nameCourse ; 
+    var tagNameCourse = document.createElement("span");
+    tagNameCourse.innerHTML = nameCourse;
 
     // course time 
-    var tagTimeCourse = document.createElement("span") ; 
-    tagTimeCourse.innerHTML = timeCourse ; 
+    var tagTimeCourse = document.createElement("span");
+    tagTimeCourse.innerHTML = timeCourse;
+
+    // add event listener on delete button 
+    tagTrashButton.addEventListener("click", function (event) {
+        deleteCourse(event.target.id);
+    })
 
     // assembly 
-    tagScoreCourse.appendChild(tagTrashButton); 
-    tagScoreCourse.appendChild(tagNameCourse); 
-    tagScoreCourse.appendChild(tagTimeCourse) ; 
+    tagScoreCourse.appendChild(tagTrashButton);
+    tagScoreCourse.appendChild(tagNameCourse);
+    tagScoreCourse.appendChild(tagTimeCourse);
 
-    return tagScoreCourse ; 
-    
+    return tagScoreCourse;
+
 
 }
-function drawListCourse()
-{  
-    tagPrintZone.innerHTML = "" ; 
-    listeCourse.forEach(function(course){
+function drawListCourse() {
+    tagPrintZone.innerHTML = "";
+    listeCourse.forEach(function (course, index) {
         var time = new Date(course.time)
-        var sec = time.getSeconds() ; 
-        var min = time.getMinutes() ; 
-        var hour = time.getUTCHours() ; 
-        stringTime = hour + ":" + min + ":" + sec; 
-        tagPrintZone.appendChild(createTagCourseTime(course.name, stringTime)); 
+        var sec = time.getSeconds();
+        var min = time.getMinutes();
+        var hour = time.getUTCHours();
+        stringTime = hour + ":" + min + ":" + sec;
+        tagPrintZone.appendChild(createTagCourseTime(course.name, stringTime, index));
     })
 }
 
@@ -174,9 +187,11 @@ tagButtonRun.addEventListener("click", function () {
 
 });
 
-tagButtonSubmitSave.addEventListener("click", function(){
-    saveCourse() ; 
+tagButtonSubmitSave.addEventListener("click", function () {
+    saveCourse();
 })
 
 
 
+// after DOM is load 
+loadDataCourses(); 
